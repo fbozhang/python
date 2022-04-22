5d80493be21b9ad26dcdabedf2ed86f994a789fd
fbz <fbozhang@163.com>
2022-04-22 22:56:57 +0800

    调整格式

--- spider/ThreadPool/ThreadPool.py
+++ spider/ThreadPool/ThreadPool.py
@@ -13,6 +13,7 @@ from concurrent.futures import ThreadPoolExecutor
 f = open("data.csv", mode="w", encoding="utf-8")
 csvwriter = csv.writer(f)
 
+
 def html(current):
     url = "http://www.xinfadi.com.cn/getPriceData.html"
     data = {
@@ -47,15 +48,13 @@ def html(current):
     print(url, "提取完毕")
 
 
-
-
 if __name__ == '__main__':
     # for i in range(1, 14900):   # 单线程效率极低
     #     html(i)
 
     # 创建线程池
     with ThreadPoolExecutor(50) as t:
-        for i in range(1, 100):   # 99*20个数据
+        for i in range(1, 100):  # 99*20个数据
             # 把下载任务提交给线程池
             t.submit(html, i)
     print("over!!")
