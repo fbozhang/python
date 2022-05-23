0d9e19784ce4813349f0982a73c9b2e5fa29e217
fbz <fbozhang@163.com>
2022-05-24 01:16:19 +0800

    v1.03新增事件处理:     点击关闭按钮，推出程序的事件     方向控制，子弹发射 获取键盘按键还未实现

--- pygame/tank/tankGame.py
+++ pygame/tank/tankGame.py
@@ -3,17 +3,12 @@
 # @Author: fbz
 # @File : tankGame.py
 
-# -*- coding:utf-8 -*-
-# @Time : 2022/5/23 20:40
-# @Author: fbz
-# @File : tankGame.py
-
 """
-v1.02
+v1.03
     新增功能:
-        创建游戏窗口
-        用到游戏引擎中的功能模块
-        官方开发文档
+    事件处理:
+        点击关闭按钮，推出程序的事件
+        方向控制，子弹发射
 """
 
 import pygame
@@ -33,7 +28,6 @@ class MainGame():
 
     # 开始游戏方法
     def startGame(self):
-        pass
         # 创建窗口加载窗口
         MainGame.window = pygame.display.set_mode([MainGame.SCREEN_WIDTH, MainGame.SCREEN_HEIGHT])
         # 设置游戏标题
@@ -42,12 +36,40 @@ class MainGame():
         while True:
             # 给窗口填充颜色
             MainGame.window.fill(COLOR_BLACK)
+            # 在循环中持续完成事件的获取
+            self.getEvent()
             # 窗口的刷新
             pygame.display.update()
 
+    # 获取程序运行期间所有事件(鼠标事件，键盘事件)
+    def getEvent(self):
+        key = pygame.key.get_pressed()
+        # 1.获取所有事件
+        eventList = pygame.event.get()
+        # 2.对事件进行判断处理(1. 点击关闭按钮  2.按下键盘上的某个键)
+        for event in eventList:
+            # 判断event.type  是否QUIT，如果是退出的话，直接调用程序结束方法
+            if event.type == pygame.QUIT:
+                self.endGame()
+            # 判断事件类型是否为按键按下，如果是:继续判断按键是哪一个按键来进行对应的处理
+            if event.type == pygame.K_DOWN:
+                # 具体是哪一个按键的处理
+                if event.key == pygame.K_LEFT:
+                    print("坦克向左调头, 移动")
+                elif event.key == pygame.K_RIGHT:
+                    print("坦克向右调头, 移动")
+                elif event.key == pygame.K_UP:
+                    print("坦克向上调头, 移动")
+                elif event.key == pygame.K_DOWN:
+                    print("坦克向下调头, 移动")
+                elif event.key == pygame.K_SPACE:
+                    print("发射子弹")
+
     # 结束游戏方法
     def endGame(self):
-        pass
+        print("谢谢使用")
+        # 结束python解释器
+        exit()
 
 
 class Tank():
