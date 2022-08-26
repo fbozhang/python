2c60639532e29692929a325301f00e02ec25f3e5
fbz <fbozhang@163.com>
2022-08-26 17:05:26 +0800

    完善人员管理系统 校验图片验证码

--- staffSystem_django/app01/views/account.py
+++ staffSystem_django/app01/views/account.py
@@ -30,6 +30,11 @@ class LoginForm(BootStrapForm):
         widget=forms.PasswordInput(render_value=True),
         required=True
     )
+    code = forms.CharField(
+        label='用户名',
+        widget=forms.TextInput,
+        required=True  # 必填(默认为True，这里不写也行)
+    )
 
     def clean_password(self):
         pwd = self.cleaned_data.get('password')
@@ -46,7 +51,14 @@ def login(request):
     form = LoginForm(data=request.POST)
     if form.is_valid():
         # 验证成功，获取到的用户名和密码
-        # print(form.cleaned_data)  # {'username': 'asd', 'password': '8dcc02a3be062a857c2252db1f36866a'}
+        # print(form.cleaned_data)  # {'username': 'asd', 'password': '8dcc02a3be062a857c2252db1f36866a','code':asdd}
+
+        # 验证码校验
+        user_input_code = form.cleaned_data.pop('code')  # pop()得到后在字典中去掉这个键值对
+        code = request.session.get('img_code', '')  # 获取不到默认为空字符串
+        if code.upper() != user_input_code.upper():  # upper()把字符串统一变成大写
+            form.add_error('code', '验证码错误')
+            return render(request, 'login.html', {'form': form})
 
         # 去数据库校验用户名和密码是否正确,获取用户对象、None
         # Admin.objects.filter(username=form.cleaned_data['username'],password=form.cleaned_data['password']).first()
@@ -59,6 +71,9 @@ def login(request):
         # 用户名和密码正确
         # 网站生成随机字符串；写到用户浏览器的cookie中；再写入到session中
         request.session['info'] = {'id': admin_object.id, 'name': admin_object.username}
+        # session可以保存7天(7天免登录)
+        request.session.set_expiry(60 * 60 * 24 * 7)
+
         return redirect('/admin/list/')
 
     return render(request, 'login.html', {'form': form})
@@ -70,8 +85,14 @@ def image_code(request):
 
     # 调用pillow函数生成图片
     img, code_str = check_code()
-    print(code_str)
+    # print(code_str)
 
+    # 写入到session中以便后续获取验证码进行校验
+    request.session['img_code'] = code_str
+    # 给session设置60s超时
+    request.session.set_expiry(60)
+
+    # 写入内存
     stream = BytesIO()
     img.save(stream, 'png')
 
