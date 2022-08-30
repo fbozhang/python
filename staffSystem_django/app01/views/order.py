988a665cb66833ad13402e11429ae68346f8b0d6
fbz <fbozhang@163.com>
2022-08-30 16:18:23 +0800

    完善人员管理系统 订单ajax添加（显示列表）

--- staffSystem_django/app01/views/order.py
+++ staffSystem_django/app01/views/order.py
@@ -11,19 +11,43 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from app01.models import *
+from app01.utils.pagination import Pagination
 from app01.utils.bootstrap import BootStrapModelForm
 
 
 class OrderModelForm(BootStrapModelForm):
     class Meta:
         model = Order
-        fields = '__all__'
-        # exclude = ['oid']
-        
+        # fields = '__all__'
+        exclude = ['admin']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        value = datetime.now().strftime('%Y%m%d%H%M%S') + str(random.randint(100, 999))
+        # 循环找到所有插件，添加class="form-control"
+        for name, field in self.fields.items():
+            if name == "oid":
+                field.widget.attrs = {
+                    "value": value,
+                    "class": "form-control",
+                    "placeholder": field.label,
+                    # 'disabled': "disabled", # 不可编辑，不可复制，不可选择，不能接收焦点,后台也不会接收到传值
+                    "readonly": "readonly",  # 只读可复制，可选择,可以接收焦点，可以选中或拷贝其文本。后台会接收到传值
+                }
+
 
 def order_list(request):
+    queryset = Order.objects.all().order_by('-id')
+    page_object = Pagination(request, queryset)
+
     form = OrderModelForm()
-    return render(request, 'order_list.html', {'form': form})
+    context = {
+        'form': form,
+        'queryset': page_object.page_queryset,  # 分页数据
+        'page_string': page_object.html(),  # 页码
+    }
+    return render(request, 'order_list.html', context)
 
 
 @csrf_exempt
@@ -33,7 +57,11 @@ def order_add(request):
     # 用户发送过来的数据进行校验(ModelForm)
     form = OrderModelForm(data=request.POST)
     if form.is_valid():
-        form.instance.oid = datetime.now().strftime('%Y%m%d%H%M%S') + str(random.randint(100, 999))
+        # 订单号默认值（也可以使用上面构造方法的方式设置）
+        # form.instance.oid = datetime.now().strftime('%Y%m%d%H%M%S') + str(random.randint(100, 999))
+        # 设置管理员ID为当前用户ID
+        form.instance.admin_id = request.session['info']['id']
+
         # 保存到数据库中
         form.save()
         data_dict = {'status': True}
