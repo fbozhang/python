767e932336da973761f57297f82cb0955e38258f
fbz <fbozhang@163.com>
2022-09-05 18:19:57 +0800

    在线商城 配置log

--- mall/mall_django/mall_django/urls.py
+++ mall/mall_django/mall_django/urls.py
@@ -16,6 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+
+def log(request):
+    # 1. 导入
+    import logging
+    # 2. 创建日志器
+    logger = logging.getLogger('django')
+    # 3. 调动日志器的方法来保存日志
+    logger.info('用户登录了')
+    logger.warning('redis缓存不足')
+    logger.error('该记录不存在')
+
+    return HttpResponse('log')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('log/', log),
 ]
