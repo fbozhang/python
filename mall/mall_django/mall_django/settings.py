ca3a0c02357711c54a9f64e1c115acdd4b136ead
fbz <fbozhang@163.com>
2022-09-05 17:12:55 +0800

    在线商城 django-redis配置

--- mall/mall_django/mall_django/settings.py
+++ mall/mall_django/mall_django/settings.py
@@ -121,3 +121,25 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django-redis
+# https://django-redis-chs.readthedocs.io/zh_CN/latest/
+
+CACHES = {
+    "default": {  # 预留
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": {  # 用于保存session数据
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
