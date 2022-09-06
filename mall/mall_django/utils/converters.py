395cb8df4103721818dde9ff790c625e0e7ca575
fbz <fbozhang@163.com>
2022-09-06 21:03:10 +0800

    在线商城 实现注册，各种参数的校验，手机号转换器，去除csrf校验

--- mall/mall_django/utils/converters.py
+++ mall/mall_django/utils/converters.py
@@ -5,8 +5,24 @@
 
 from django.urls import converters
 
+
+# 验证用户名规则
 class UsernameConverter:
     regex = '[a-zA-Z0-9_-]{5,20}'
 
     def to_python(self, value):
         return value
+
+    def to_url(self, value):
+        return str(value)
+
+
+# 验证手机号规则
+class MobileConverter:
+    regex = '1[345789]\d{9}'
+
+    def to_python(self, value):
+        return value
+
+    def to_url(self, value):
+        return str(value)
