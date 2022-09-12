1644354f9c8081aa5805f40053f1413ac63393dd
fbz <fbozhang@163.com>
2022-09-12 23:20:35 +0800

    在线商城 验证用户是否登陆

--- mall/mall_django/utils/views.py
+++ mall/mall_django/utils/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 
 
+# 验证用户是否登陆
+# LoginRequiredMixin未登錄是返回重定向，前後端分離需要返回json
 class LoginRequiredJsonMixin(LoginRequiredMixin):
     # 重寫方法
     def handle_no_permission(self):
