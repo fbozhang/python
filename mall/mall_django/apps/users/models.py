b994166ce7a8d1346596f0bafd9065fb8bbf862e
fbz <fbozhang@163.com>
2022-09-14 23:46:23 +0800

    在线商城 地址模型

--- mall/mall_django/apps/users/models.py
+++ mall/mall_django/apps/users/models.py
@@ -18,8 +18,36 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     mobile = models.CharField(max_length=11, unique=True)
     email_active = models.BooleanField(default=False, verbose_name='郵箱驗證狀態')
+    default_address = models.ForeignKey('Address', related_name='users', null=True, blank=True,
+                                        on_delete=models.SET_NULL, verbose_name='默认地址')
 
     class Meta:
         db_table = 'tb_users'
         verbose_name = '用户管理'
         verbose_name_plural = verbose_name
+
+
+from utils.models import BaseModel
+
+
+class Address(BaseModel):
+    """用户地址"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name='用户')
+    title = models.CharField(max_length=20, verbose_name='地址名称')
+    receiver = models.CharField(max_length=20, verbose_name='收货人')
+    province = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='province_addresses',
+                                 verbose_name='省')
+    city = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='city_addresses', verbose_name='市')
+    district = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='district_addresses',
+                                 verbose_name='区')
+    place = models.CharField(max_length=50, verbose_name='地址')
+    mobile = models.CharField(max_length=11, verbose_name='手机')
+    tel = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='固定电话')
+    email = models.CharField(max_length=30, null=True, blank=True, default='', verbose_name='电子邮箱')
+    is_deleted = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+    class Meta:
+        db_table = 'tb_address'
+        verbose_name = '用户地址'
+        verbose_name_plural = verbose_name
+        ordering = ['-update_time']
