9843e8a9e947431451db93ab5badeda58b485b2a
fbz <fbozhang@163.com>
2022-09-20 03:34:27 +0800

    在线商城 修改BigAutoField 为AutoField 使自動生成的主鍵為int型而不是bigint型

--- mall/mall_django/apps/verifications/apps.py
+++ mall/mall_django/apps/verifications/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class VerificationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.AutoField'
     name = 'apps.verifications'
