664f2b7642af14fe2728b0c5631c858d95a6dff1
fbz <fbozhang@163.com>
2023-03-25 02:48:34 +0800

    drf:序列化器的使用 外键字段的第三种定义方法，serializers.StringRelatedField

--- drfTest/app01/serializers.py
+++ drfTest/app01/serializers.py
@@ -31,10 +31,19 @@ class CountrySerializers(serializers.Serializer):
     # TypeError: int() argument must be a string, a bytes-like object or a number, not 'Area'
 
     # 方法1
+    # 如果定义的序列化起外键字段类型为 IntegerField
+    # 那么定义的序列化器字段名 必须和数据库中的外键字段名一致
     # area_id = serializers.IntegerField()
 
     # 方法2
+    # 如果想要外键数据的key就是模型字段名字，那么PrimaryKeyRelatedField 就可以获取到关联的模型id
+    # queryset 再验证数据的时候告诉系统在哪匹配外键数据
     # AssertionError: Relational field must provide a `queryset` argument, override `get_queryset`, or set read_only=`True`.
-    area = serializers.PrimaryKeyRelatedField(queryset=Area.objects.all())
+    # area = serializers.PrimaryKeyRelatedField(queryset=Area.objects.all())
     # 或者 read_only=True,意思是不验证了
     # area = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    # 方法3
+    # 如果想要获取外键字段关联的 字符串信息，可以使用 StringRelatedField
+    # 实际上获取的是模型中 def __str__(self): 的返回值
+    area = serializers.StringRelatedField()
