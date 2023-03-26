a4a3e035cc4750dd84bfe645a940be33211bc574
fbz <fbozhang@163.com>
2023-03-26 21:04:20 +0800

    drf:视图集的使用

--- drfTest/drfTest/urls.py
+++ drfTest/drfTest/urls.py
@@ -36,14 +36,31 @@ urlpatterns = [
 ]
 
 # 视图集的路由借助于 drf 的 router 实现
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
+
+"""
+DefaultRouter, SimpleRouter
+共同点: 都可以帮助 视图集自动生成路由
+不同点:
+    DefaultRouter   http://127.0.0.1:8000/  根路由可以访问
+    SimpleRouter    http://127.0.0.1:8000/  根路由不可以访问
+"""
 
 # 创建router实例
-router = DefaultRouter()
+router = SimpleRouter()
 # 设置列表视图和详情视图的公共部分(不包括/部分)
-# prefix            列表视图和详情视图的公共部分(不包括/部分)
+# prefix            路由，列表视图和详情视图的公共部分(不包括最后的/部分)
+#                   router 会生成2个路由，一个是列表视图的路由 prefix ，另一个是详情视图的路由 prefix/pk/
 # viewset           视图集
 # basename=None     给列表视图和详情视图的路由设置别名
+#                   别名的规范是 列表视图是: basename-list   详情视图是: basename-detail
+#                   因为别名是原因，所以 basename 不要重复，一般以 prefix 作为 basename，因为 prefix不会重复
 router.register(prefix='asd', viewset=CountryModelViewSet, basename='qwe')
 # 将router生成的路由追加到 urlpatterns
 urlpatterns += router.urls
+# 或者下面这样写
+'''
+urlpatterns = [
+    path('', include(router.urls)),
+]
+'''
