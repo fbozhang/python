f0bf873cbf59bec975e011d5566bfe36be6d8989
fbz <fbozhang@163.com>
2023-07-30 22:35:42 +0800

    修改parse方法，使得pycharm不再警告并且可以有提示

--- spider/scrapy/game_4399/game_4399/spiders/small_game.py
+++ spider/scrapy/game_4399/game_4399/spiders/small_game.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.http.response import Response
 
 
 class SmallGameSpider(scrapy.Spider):
@@ -6,7 +7,7 @@ class SmallGameSpider(scrapy.Spider):
     allowed_domains = ["4399.com"]  # 允许的域名
     start_urls = ["https://www.4399.com/"]  # 起始页面的URL
 
-    def parse(self, response):  # 该方法默认是用来处理解析的
+    def parse(self, response: Response, **kwargs):  # 该方法默认是用来处理解析的
         # 拿到页面源代码
         # print(response.text)
         # 提取数据
@@ -22,7 +23,7 @@ class SmallGameSpider(scrapy.Spider):
         for li in li_list:
             # name = li.xpath('./a/text()').extract()[0] # 如果列表为空取0会报错
             # extract() 返回列表, extract_first() 返回第一项数据
-            name = li.xpath('./a/text()').extract_first() # extract_first提取一项内容，如果没有返回 None
+            name = li.xpath('./a/text()').extract_first()  # extract_first提取一项内容，如果没有返回 None
             img = li.xpath('./a/img/@lz_src').extract_first()
 
             data_dict = {
@@ -30,5 +31,4 @@ class SmallGameSpider(scrapy.Spider):
                 'img': img,
             }
             # 需要用yield将数据传递给管道, 不是装在列表一次性return使用yield省内存
-            yield data_dict# 如果返回的是数据，直接可以认为是给了管道pipeline(给了引擎，引擎给管道)
-
+            yield data_dict  # 如果返回的是数据，直接可以认为是给了管道pipeline(给了引擎，引擎给管道)
