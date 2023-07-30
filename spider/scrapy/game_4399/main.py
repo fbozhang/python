3e6053f3240c0aa7db727078c120ca7a88dd02e4
fbz <fbozhang@163.com>
2023-07-30 22:44:31 +0800

    使用scrapy genspider -t mytemplate 爬虫名字 爬虫域名 创建爬虫

--- spider/scrapy/game_4399/main.py
+++ spider/scrapy/game_4399/main.py
@@ -6,6 +6,7 @@ from scrapy.cmdline import execute
 import os
 import sys
 
+# scrapy genspider -t mytemplate 爬虫名字 爬虫域名
 if __name__ == '__main__':
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
     execute(['scrapy', 'crawl', 'small_game'])  # 把最后的参数换成你自己的爬虫名字
