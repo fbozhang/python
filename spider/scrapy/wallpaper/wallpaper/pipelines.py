79e1108755357c555729952195e83a6e7d85d19a
fbz <fbozhang@163.com>
2023-08-06 01:26:12 +0800

    增量式爬取

--- spider/scrapy/wallpaper/wallpaper/pipelines.py
+++ spider/scrapy/wallpaper/wallpaper/pipelines.py
@@ -6,9 +6,22 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from redis import Redis
+import json
 
 
 class WallpaperPipeline:
     def process_item(self, item, spider):
         print('收到数据', item)
+        res = self.redis.sadd('wallpaper:detail:item', json.dumps(dict(item)))  # 抓取完了就存redis，免得重复抓取
+        if res:
+            print('存入数据库')
+        else:
+            print('数据已存在在数据库中')
         return item
+
+    def open_spider(self, spider):
+        self.redis = Redis(host='127.0.0.1', port=6379, db=5)
+
+    def close_spider(self, spider):
+        self.redis.close()
--- spider/scrapy/wallpaper/wallpaper/spiders/pic.py
+++ spider/scrapy/wallpaper/wallpaper/spiders/pic.py
@@ -1,6 +1,7 @@
 import scrapy
 from scrapy.http.response import Response
 from wallpaper.items import WallpaperItem
+from redis import Redis
 
 
 class PicSpider(scrapy.Spider):
@@ -8,12 +9,24 @@ class PicSpider(scrapy.Spider):
     allowed_domains = ["pic.netbian.com"]
     start_urls = ["https://pic.netbian.com"]
 
+    def __init__(self, name=None, **kwargs):
+        super(PicSpider, self).__init__(name, **kwargs)
+        self.redis = Redis(host='127.0.0.1', port=6379, db=5)
+
     def parse(self, response: Response, **kwargs):
         li_list = response.xpath('//ul[@class="clearfix"]/li')[:-1]
         for li in li_list:
             href = li.xpath('./a/@href').extract_first()
             detail_url = response.urljoin(href)
-            yield scrapy.Request(detail_url, callback=self.parse_detail)
+            # 数据去重
+            # 1. url，优点: 简单，缺点: 如果url会更新可能造成重复
+            # 判断redis里有没有这个url，只有redis里面没有url才访问详情页，防止重复抓取
+            # 2. 判断数据是否重复，优点: 保证数据的一致性，缺点: 数据量大的时候，效率低，对redis不利因为redis本质在内存(在pipelines中写)
+            result = self.redis.sismember('wallpaper:detail:url', detail_url)
+            if result:
+                print(f'该详情页已经被抓取过了{detail_url}')  # 不管他就完事了
+            else:
+                yield scrapy.Request(detail_url, callback=self.parse_detail)
 
         # next_url = response.xpath('//div[@class="page"]/a[last()]/@href').extract_first()
         next_url = response.xpath('//div[@class="page"]/a[contains(text(),"下一页")]/@href').extract_first()
@@ -25,4 +38,5 @@ class PicSpider(scrapy.Spider):
         src = response.xpath('//a[@id="img"]/img/@src').extract_first()
         img = response.urljoin(src)
         w['img'] = img
+        self.redis.sadd('wallpaper:detail:url', response.url)  # 抓取完了就存redis，免得重复抓取
         yield w
