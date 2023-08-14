6c9f8fdb48be28d1f1d8c02b6b3021014e6d24c8
fbz <fbozhang@163.com>
2023-08-15 00:10:44 +0800

    使用selenium中间件解决Ajax动态渲染

--- spider/scrapy/douban/douban/middlewares.py
+++ spider/scrapy/douban/douban/middlewares.py
@@ -3,13 +3,45 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 import base64
+import time
 
 from scrapy import signals
 import random
+from selenium import webdriver
+from scrapy.http import HtmlResponse
 from douban.settings import USER_AGENT_LIST, PROXY_LIST
+
+"""
+中间件有三种返回
+1. 返回response -> 给爬虫
+2. 返回request ->  给调度器
+3. 返回None -> 给下载器，默认是None
+"""
+
+
+class seleniumMIddleware(object):
+    """ selenium中间件(解决ajax动态渲染网页) """
+
+    def process_request(self, request, spider):
+        if spider.name == 'movie':
+            print('使用selenium')
+            url = request.url
+            if '250' in url:  # 看哪个url需要用selenium等页面渲染完再拿响应
+                driver = webdriver.Chrome()
+
+                driver.get(url)
+                time.sleep(3)  # 等页面渲染完，可以判断出现了哪些元素就是渲染完
+                data = driver.page_source  # 获取渲染完的页面
+
+                driver.close()
+
+                return HtmlResponse(url=url, body=data, encoding='utf-8', request=request)  # 返回给爬虫去解析
+            else:
+                return None  # 返回给下载器，不写else也可以，默认返回None
 
 
 class RandomUserAgentMiddleware(object):
+    """ 随机UA中间件 """
 
     def process_request(self, request, spider):
         print(request.headers['User-Agent'])
@@ -18,6 +50,7 @@ class RandomUserAgentMiddleware(object):
 
 
 class RandomProxyMiddleware(object):
+    """ 随机代理中间件 """
 
     def process_request(self, request, spider):
         proxy = random.choice(PROXY_LIST)
